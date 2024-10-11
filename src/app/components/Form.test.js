0bde9a18f2b68dfe1ed508e8daae9bf2c05f75b0
Vitor Menezes Marques <vitor.marques@compasso.com.br>
2024-10-11 11:18:06 -0300

    test: updating test date

--- src/app/components/Form.test.js
+++ src/app/components/Form.test.js
@@ -6,7 +6,6 @@ describe('Form Component', () => {
   test('renders form and submits successfully', () => {
     render(<Form />);
 
-    // Verifica se os campos estão presentes
     expect(screen.getByLabelText(/Nome Completo/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/E-mail/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/Data de Nascimento/i)).toBeInTheDocument();
@@ -14,33 +13,29 @@ describe('Form Component', () => {
     expect(screen.getByLabelText(/Telefone/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/Sexo/i)).toBeInTheDocument(); 
 
-    // Preenche o formulário
     fireEvent.change(screen.getByLabelText(/Nome Completo/i), {
-      target: { value: 'João Silva' },
+      target: { value: 'vitor menezes' },
     });
     fireEvent.change(screen.getByLabelText(/E-mail/i), {
-      target: { value: 'joao.silva@example.com' },
+      target: { value: 'vitor.menezes@test.com' },
     });
     fireEvent.change(screen.getByLabelText(/Data de Nascimento/i), {
-      target: { value: '2000-01-01' },
+      target: { value: '2001-05-19' },
     });
     fireEvent.change(screen.getByLabelText(/Naturalidade/i), {
-      target: { value: 'São Paulo' },
+      target: { value: 'Brsileiro' },
     });
     fireEvent.change(screen.getByLabelText(/Telefone/i), {
-      target: { value: '(11) 91234-5678' },
+      target: { value: '(88) 91234-5678' },
     });
     fireEvent.change(screen.getByLabelText(/Sexo/i), {
       target: { value: 'masculino' },
     });
 
-    // Submete o formulário
     fireEvent.click(screen.getByRole('button', { name: /Enviar/i }));
 
-    // Verifica se a mensagem de sucesso é exibida
     expect(screen.getByText(/Dados enviados com sucesso!/i)).toBeInTheDocument();
 
-    // Verifica se os campos foram limpos
     expect(screen.getByLabelText(/Nome Completo/i).value).toBe('');
     expect(screen.getByLabelText(/E-mail/i).value).toBe('');
     expect(screen.getByLabelText(/Data de Nascimento/i).value).toBe('');
