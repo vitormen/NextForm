3ee9c164b4f24923f5f613295f47c350b8e30c0c
vitor menezes <141634790+vitormen@users.noreply.github.com>
2024-11-04 08:18:12 -0300

    feat: date page created

---
+++ src/app/date/page.js
@@ -0,0 +1,15 @@
+import Navbar from "../components/navbar/NavBar"
+import FormDataComponent from "../components/formDataComponent/FormDataComponent"
+import Footer from "../components/footer/Footer"
+const date = () => {
+  return (
+    <div>
+        <Navbar/>
+        <FormDataComponent/>
+        <Footer/>
+        </div>
+    
+  )
+}
+
+export default date
